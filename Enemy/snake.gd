fa937346d88a84b062e7b20c9ba7ad80e8d3b6e8
Askey <aspe@summacollege.nl>
2026-09-09 16:10:20 +0200

    wip snake raycasts

--- Enemy/snake.gd
+++ Enemy/snake.gd
@@ -1,8 +1,16 @@
-extends CharacterBody2D
+class_name Snake extends CharacterBody2D
+
+@onready var _raycasts := %Raycasts
+
 var is_following := false
 var player
-var speed :float = 200.0
+var speed :float = 3000.0
 func _process(delta: float)->void:
+	
+	var sees_player := false
+	for ray : RayCast2D in _raycasts.get_children():
+		if ray.is_colliding():
+			var collision
 	if is_following && player != null:
 		var direction_to_player := global_position.direction_to(player.global_position)
 
@@ -12,12 +20,3 @@ func _process(delta: float)->void:
 		return
 	
 	#Look around
-
-func _on_follow_range_body_entered(body: Node2D) -> void:
-	if body is Player:
-		is_following = true
-		player = body
-
-func _on_follow_range_body_exited(body: Node2D) -> void:
-	is_following = false
-	player = null
